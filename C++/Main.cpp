bf16c2931f1a4d0ce6f863ce3383e6e4e4089fd5
kasi-sj <kasinathansj21cs@psnacet.edu.in>
2024-02-25 17:43:43 +0530

    lower_bound_added

--- C++/Main.cpp
+++ C++/Main.cpp
@@ -1,7 +1,7 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long int
- 
+
 #define MOD 1000000007
 #define LOG 21
 #define MIN -1000000000
@@ -10,90 +10,124 @@ using namespace std;
 
 using namespace std;
 
-ll power[(int)2e6+2];
-
-void calculatePower(int n){
-    power[0] = 1;
-    for(int i = 1 ; i <= n ; i++){
-        power[i] = (power[i-1]*PRIME)%MOD;
-    }
-}
-
-void rollingHash(string &s , ll Hash[]){
-    int n = s.size();
-    Hash[0] = s[0];
-    for(int i = 1 ; i < n ; i++){
-        Hash[i] = (Hash[i-1]*PRIME + s[i])%MOD;
-    }
-    return ;
-}
-
-ll getHash(int l , int r , ll Hash[]){
-    if(l==0)return Hash[r];
-    else{
-        return (MOD + Hash[r] - (Hash[l-1]*power[r-l+1])%MOD)%MOD;
-    }
-}
-
-void start() {
+struct fenwik
+{
     int n;
-    int s;
+    int *fen;
+    fenwik(int n) : n(n + 1)
+    {
+        fen = new int[n + 1];
+        memset(fen, 0, sizeof(int) * (n + 1));
+    }
+
+    void update(int i, int add)
+    {
+        i++;
+        while (i < n)
+        {
+            fen[i] += add;
+            i = i + (i & (-i));
+        }
+    }
+
+    int sum(int i)
+    {
+        i++;
+        int s = 0;
+        while (i > 0)
+        {
+            s += fen[i];
+            i = i - (i & (-i));
+        }
+        return s;
+    }
+
+    int rangeSum(int l, int r)
+    {
+        return sum(r) - sum(l - 1);
+    }
+
+    int lowerBound(int val)
+    {
+        int sum = 0;
+        int pos = 0;
+        int bitmask = 1;
+        while (bitmask <= n)
+            bitmask <<= 1;
+        bitmask >>= 1;
+        for (int i = bitmask; i > 0; i >>= 1)
+        {
+            if (pos + i < n && sum + fen[pos + i] < val)
+            {
+                sum += fen[pos + i];
+                pos += i;
+            }
+        }
+        return pos + 1;
+    }
+};
+
+
+void start()
+{
+    int n;
     cin >> n;
-    cin >> s;
-    vector<int> v;
-    for(int i = 0 ; i < n ; i++){
-        int val;
-        cin >> val;
-        v.push_back(val);
+    int k;
+    cin >> k;
+    fenwik fn(n);
+    for (int i = 0; i < n; i++)
+    {
+        fn.update(i, 1);
     }
-    ll ans  = 0;
-    map<ll,int> mp;
-    mp[0] = 1;
-    ll sum = 0;
-    for(int i = 0 ; i < n ; i++){
-        sum += v[i];
-        if(mp.count(sum-s)!=0){
-            ans += mp[sum-s];
+    int p = 0;
+    for (int i = 0; i < n; i++)
+    {
+        p = (p + k) % (n - i);
+        int lo = 0;
+        int hi = n-1;
+        int ans = -1;
+        while(lo<=hi){
+            int mid = (lo+hi)>>1;
+            if(fn.sum(mid)>p+1)hi = mid-1;
+            else if(fn.sum(mid)==p+1){
+                ans = mid;
+                hi = mid-1;
+            }
+            else lo = mid+1;
         }
-        if(mp.count(sum)!=0){
-            mp[sum]++;
-        }else{
-            mp[sum] = 1;
-        }
-        // for(auto e : mp){
-        //     cout << e.first << " "<< e.second << "-";
-        // }
-        // cout << endl;
-        // cout << sum << " " << ans << endl;
+        cout << fn.lowerBound(p+1) << " ";
+        fn.update(ans,-1);
     }
-    cout << ans << endl;
 }
 
 
-
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
     ifstream inputFile("input.txt");
-    if (inputFile.is_open()) {
+    if (inputFile.is_open())
+    {
         cin.rdbuf(inputFile.rdbuf());
     }
 
     ofstream outputFile("output.txt");
-    if (outputFile.is_open()) {
+    if (outputFile.is_open())
+    {
         cout.rdbuf(outputFile.rdbuf());
     }
- 
+
     int n = 1;
     // cin >> n;
-    while (n-- > 0) {
+    while (n-- > 0)
+    {
         start();
     }
- 
+
     inputFile.close();
     outputFile.close();
-    
+
     return 0;
 }
--- JAVA/Special/Fenwick.java
+++ JAVA/Special/Fenwick.java
@@ -1,40 +1,56 @@
-
-class Fenwick{
+class Fenwick {
     int n;
     int arr[];
     int org[];
-    Fenwick(int n){
-        this.n = n+1;
-        this.arr = new int[n+1];
-        this.org = new int[n+1];
+
+    Fenwick(int n) {
+        this.n = n + 1;
+        this.arr = new int[n + 1];
+        this.org = new int[n + 1];
     }
 
-    void add(int i , int e){
+    void add(int i, int e) {
         i++;
-        while(i<n){
-            arr[i]+=e;
-            i+=(i&(-i));
+        while (i < n) {
+            arr[i] += e;
+            i += (i & (-i));
         }
     }
 
-    int sum(int i){
+    int sum(int i) {
         i++;
         int sum = 0;
-        while(i>0){
-            sum+=arr[i];
-            i-=(i&(-i));
+        while (i > 0) {
+            sum += arr[i];
+            i -= (i & (-i));
         }
         return sum;
     }
 
-    int range(int fr , int to){
-        return sum(to)-sum(fr-1);
+    int range(int fr, int to) {
+        return sum(to) - sum(fr - 1);
     }
 
-    void update(int i , int e){
+    void update(int i, int e) {
         i++;
-        int up = e-org[i];
-        org[i]=e;
-        add(i-1,up);
+        int up = e - org[i];
+        org[i] = e;
+        add(i - 1, up);
+    }
+
+    int lowerBound(int val) {
+        int sum = 0;
+        int pos = 0;
+        int bitmask = 1;
+        while (bitmask <= n)
+            bitmask <<= 1;
+        bitmask >>= 1;
+        for (int i = bitmask; i > 0; i >>= 1) {
+            if (pos + i < n && sum + arr[pos + i] < val) {
+                sum += arr[pos + i];
+                pos += i;
+            }
+        }
+        return pos + 1;
     }
 }
