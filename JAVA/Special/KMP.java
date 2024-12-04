e62b3cd6d1e1a47ee2404a1a61bf1372aed63102
kasi-sj <kasinathansj21cs@psnacet.edu.in>
2024-12-04 07:26:18 +0530

    update

--- JAVA/Special/KMP.java
+++ JAVA/Special/KMP.java
@@ -6,22 +6,11 @@ class KMP{
     public static int[] computeLPS(String pat){
         int n = pat.length();
         int lps[] = new int[n];
-        lps[0] = 0;
-        int i = 1;
-        int pre = 0;
-        while( i < n){
-            if(pat.charAt(i) == pat.charAt(pre)){
-                pre++;
-                lps[i] = pre;
-                i++;
-            }else{
-                if(pre!=0){
-                    pre = lps[pre-1];
-                }else{
-                    lps[i] = 0;
-                    i++;
-                }
-            }
+        for(int i = 1 ; i < n ; i++){
+            int j = lps[i-1];
+            while(j>0 && pat.charAt(i) != pat.charAt(j)) j = lps[j-1];
+            if(pat.charAt(i) == pat.charAt(j)) j++;
+            lps[i] = j;
         }
         return lps;
     }
